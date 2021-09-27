428660b6cf63a05e3fddc503b105416d98a1e9e6
rmardian <mardian@bu.edu>
2021-09-28 00:36:52 +0100

    added PyMC3 alternative model

--- example.py
+++ example.py
@@ -1,30 +1,86 @@
-import stan
+import numpy as np
+import sunode
+import sunode.wrappers.as_theano
+import pymc3 as pm
 
-schools_code = """
-data {
-  int<lower=0> J;         // number of schools
-  real y[J];              // estimated treatment effects
-  real<lower=0> sigma[J]; // standard error of effect estimates
-}
-parameters {
-  real mu;                // population treatment effect
-  real<lower=0> tau;      // standard deviation in treatment effects
-  vector[J] eta;          // unscaled deviation from mu by school
-}
-transformed parameters {
-  vector[J] theta = mu + tau * eta;        // school treatment effects
-}
-model {
-  target += normal_lpdf(eta | 0, 1);       // prior log-density
-  target += normal_lpdf(y | theta, sigma); // log-likelihood
-}
-"""
+times = np.arange(1900,1921,1)
+lynx_data = np.array([
+    4.0, 6.1, 9.8, 35.2, 59.4, 41.7, 19.0, 13.0, 8.3, 9.1, 7.4,
+    8.0, 12.3, 19.5, 45.7, 51.1, 29.7, 15.8, 9.7, 10.1, 8.6
+])
+hare_data = np.array([
+    30.0, 47.2, 70.2, 77.4, 36.3, 20.6, 18.1, 21.4, 22.0, 25.4,
+    27.1, 40.3, 57.0, 76.6, 52.3, 19.5, 11.2, 7.6, 14.6, 16.2, 24.7
+])
 
-schools_data = {"J": 8,
-                "y": [28,  8, -3,  7, -1,  1, 18, 12],
-                "sigma": [15, 10, 16, 11,  9, 11, 10, 18]}
+def lotka_volterra(t, y, p):
+    """Right hand side of Lotka-Volterra equation.
 
-posterior = stan.build(schools_code, data=schools_data)
-fit = posterior.sample(num_chains=4, num_samples=1000)
-eta = fit["eta"]  # array with shape (8, 4000)
-df = fit.to_frame()  # pandas `DataFrame, requires pandas
+    All inputs are dataclasses of sympy variables, or in the case
+    of non-scalar variables numpy arrays of sympy variables.
+    """
+    return {
+        'hares': p.alpha * y.hares - p.beta * y.lynx * y.hares,
+        'lynx': p.delta * y.hares * y.lynx - p.gamma * y.lynx,
+    }
+
+
+with pm.Model() as model:
+    hares_start = pm.HalfNormal('hares_start', sd=50)
+    lynx_start = pm.HalfNormal('lynx_start', sd=50)
+    
+    ratio = pm.Beta('ratio', alpha=0.5, beta=0.5)
+        
+    fixed_hares = pm.HalfNormal('fixed_hares', sd=50)
+    fixed_lynx = pm.Deterministic('fixed_lynx', ratio * fixed_hares)
+    
+    period = pm.Gamma('period', mu=10, sd=1)
+    freq = pm.Deterministic('freq', 2 * np.pi / period)
+    
+    log_speed_ratio = pm.Normal('log_speed_ratio', mu=0, sd=0.1)
+    speed_ratio = np.exp(log_speed_ratio)
+    
+    # Compute the parameters of the ode based on our prior parameters
+    alpha = pm.Deterministic('alpha', freq * speed_ratio * ratio)
+    beta = pm.Deterministic('beta', freq * speed_ratio / fixed_hares)
+    gamma = pm.Deterministic('gamma', freq / speed_ratio / ratio)
+    delta = pm.Deterministic('delta', freq / speed_ratio / fixed_hares / ratio)
+    
+    y_hat, _, problem, solver, _, _ = sunode.wrappers.as_theano.solve_ivp(
+        y0={
+	    # The initial conditions of the ode. Each variable
+	    # needs to specify a theano or numpy variable and a shape.
+	    # This dict can be nested.
+            'hares': (hares_start, ()),
+            'lynx': (lynx_start, ()),
+        },
+        params={
+	    # Each parameter of the ode. sunode will only compute derivatives
+	    # with respect to theano variables. The shape needs to be specified
+	    # as well. It it infered automatically for numpy variables.
+	    # This dict can be nested.
+            'alpha': (alpha, ()),
+            'beta': (beta, ()),
+            'gamma': (gamma, ()),
+            'delta': (delta, ()),
+            'extra': np.zeros(1),
+        },
+	# A functions that computes the right-hand-side of the ode using
+	# sympy variables.
+        rhs=lotka_volterra,
+	# The time points where we want to access the solution
+        tvals=times,
+        t0=times[0],
+    )
+    
+    # We can access the individual variables of the solution using the
+    # variable names.
+    pm.Deterministic('hares_mu', y_hat['hares'])
+    pm.Deterministic('lynx_mu', y_hat['lynx'])
+    
+    sd = pm.HalfNormal('sd')
+    pm.Lognormal('hares', mu=y_hat['hares'], sd=sd, observed=hare_data)
+    pm.Lognormal('lynx', mu=y_hat['lynx'], sd=sd, observed=lynx_data)
+
+with model:
+    trace = pm.sample(tune=1000, draws=1000, chains=6)
