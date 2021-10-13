96d461865ea5b0f42dec589ecac78c255c928276
rmardian <mardian@bu.edu>
2021-10-13 10:57:06 +0100

    fixed hill and again

--- PyMC3HillFunctionAND.py
+++ PyMC3HillFunctionAND.py
@@ -41,20 +41,21 @@ for gate in gates:
     with pm.Model() as bayesian_model:
         
         #sigma = pm.Normal('sigma', mu=0, sigma=1)
-        k1 = pm.Normal('K1', mu=1e2, sigma=5e1)
-        k2 = pm.Normal('K2', mu=1e2, sigma=5e1)
-        n1 = pm.Normal('n1', mu=3, sigma=1)
-        n2 = pm.Normal('n2', mu=3, sigma=1)
-        ymin1 = pm.Normal('ymin1', mu=y.min(), sigma=1)
-        ymin2 = pm.Normal('ymin2', mu=y.min(), sigma=1)
-        ymax1 = pm.Normal('ymax1', mu=y.max(), sigma=1)
-        ymax2 = pm.Normal('ymax2', mu=y.max(), sigma=1)
+        BoundNormal = pm.Bound(pm.Normal, lower=0.0)
+        k1 = BoundNormal('K1', mu=1e2, sigma=5e1)
+        k2 = BoundNormal('K2', mu=1e2, sigma=5e1)
+        n1 = BoundNormal('n1', mu=3, sigma=1)
+        n2 = BoundNormal('n2', mu=3, sigma=1)
+        ymin1 = BoundNormal('ymin1', mu=y.min(), sigma=1)
+        ymin2 = BoundNormal('ymin2', mu=y.min(), sigma=1)
+        ymax1 = BoundNormal('ymax1', mu=y.max(), sigma=1)
+        ymax2 = BoundNormal('ymax2', mu=y.max(), sigma=1)
         
         y_hat = HillFunction.hill_activation_and(x, k1, k2, n1, n2, ymin1, ymin2, ymax1, ymax2)
         y_pred = pm.Normal('y_hat', mu=y_hat, sigma=1, observed=y)
         
         step = pm.Metropolis()
-        trace = pm.sample(10000, tune=10000, cores=-1, chains=10, step=step)
+        trace = pm.sample(5000, tune=5000, cores=-1, chains=10, step=step)
 
         data = az.from_pymc3(trace=trace)
         data.to_netcdf('response-functions/hill-{}.nc'.format(gate))
